1d22f3427944c449ded358eaf83f8257adcc81c4
ander0716 <anderson.167@hotmail.com>
2022-06-11 12:17:18 -0500

    Se implementa env para encriptacion de llave secreta

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongo = require('./connection/mongoconn');
+require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))//resuelve caracteres especiales en las urls
 const port = 3001;
--- middleware/authVerify.js
+++ middleware/authVerify.js
@@ -5,7 +5,7 @@ const authVerify = (req, res, next) => {
         const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
         try {
-            const decoded = jwt.verify(token, 'millavesecreta');
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.payload = decoded;
         } catch (error) {
             return res.status(401).send('Unauthorized');
